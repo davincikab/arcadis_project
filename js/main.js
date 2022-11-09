df7850a4cec74bc3ad1bf64f24d041304e7dd432
davincikab <davidnganganjeri079@gmail.com>
2022-11-09 12:39:50 +0300

    updated to date 6

--- js/main.js
+++ js/main.js
@@ -780,5 +780,5 @@ document.onload = function(e) {
     // mapContainer.style.height = `${window.innerHeight}px`;
 }
 
-document.getElementById("date-section").innerHTML = "05/11/2022";
+document.getElementById("date-section").innerHTML = "06/11/2022";
 // document.getElementById("date-section").innerHTML = windFarmVisual.formatDate(new Date());
